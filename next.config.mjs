b7c36ae1c9fb39e2555d72f8d8259e4cf8835c26
Ahmed Alam <m.ahmed.g.1982@gmail.com>
2024-10-06 03:35:02 +0500

    FastApi Added

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    "images": { "remotePatterns": [{ "protocol": "https", "hostname": "m.media-amazon.com" }] },
+	"images": { "remotePatterns": [{ "protocol": "https", "hostname": "m.media-amazon.com" }] },
+	rewrites: async () => {
+		return [
+			{
+				source: "/api/py/:path*",
+				destination:
+					process.env.NODE_ENV === "development"
+						? "http://127.0.0.1:8000/api/py/:path*"
+						: "/api/",
+			},
+			{
+				source: "/docs",
+				destination:
+					process.env.NODE_ENV === "development"
+						? "http://127.0.0.1:8000/api/py/docs"
+						: "/api/py/docs",
+			},
+			{
+				source: "/openapi.json",
+				destination:
+					process.env.NODE_ENV === "development"
+						? "http://127.0.0.1:8000/api/py/openapi.json"
+						: "/api/py/openapi.json",
+			},
+		];
+	},
 };
 
 export default nextConfig;
